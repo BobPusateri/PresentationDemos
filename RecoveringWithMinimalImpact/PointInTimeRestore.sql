82b3c7a074762120bf5d111ac29a3adfe97af8d7
Bob Pusateri <bob@bobpusateri.com>
2021-10-18 20:51:44 -0500

    Add for PASS Summit 2021

---
+++ RecoveringWithMinimalImpact/PointInTimeRestore.sql
@@ -0,0 +1,198 @@
+-- Restore our demo data
+
+USE [master]
+GO
+
+-- RESTORE DATABASE
+RESTORE DATABASE DemoData
+FROM DISK = 'C:\Demos\AdvRestore\DemoData.bak'
+WITH REPLACE;
+
+
+
+
+-- BACKUP DATABASE
+BACKUP DATABASE DemoData
+TO DISK = 'C:\Demos\AdvRestore\DemoData_PITFull.bak'
+WITH COMPRESSION, CHECKSUM, INIT, FORMAT;
+
+-- LOG BACKUP
+BACKUP LOG DemoData
+TO DISK = 'C:\Demos\AdvRestore\DemoData_PIT1.trn'
+WITH COMPRESSION, CHECKSUM, INIT, FORMAT;
+
+
+
+
+USE [DemoData]
+GO
+
+SELECT *
+FROM dbo.ZipCodes;
+
+SELECT ZipCode, CityName, StateName
+FROM dbo.ZipCodes
+WHERE CityName = 'Brookfield'
+ORDER BY StateName;
+
+
+-- Let's change it!
+UPDATE dbo.ZipCodes
+SET ZipCode = '00000'
+WHERE CityName = 'Brookfield'
+AND StateName = 'IL';
+
+DELETE
+FROM dbo.ZipCodes
+WHERE ZipCode = '60515';
+SELECT GETDATE(); -- 2020-10-26 15:54:57.650
+
+-- Let's mess up!
+UPDATE dbo.ZipCodes
+SET StateName = 'WA'
+-- WHERE StateName = 'wa'
+SELECT GETDATE(); -- 2020-10-26 15:55:37.687
+
+
+SELECT *
+FROM dbo.ZipCodes;
+
+-- log backup
+BACKUP LOG DemoData
+TO DISK = 'C:\Demos\AdvRestore\DemoData_PIT2.trn'
+WITH COMPRESSION, CHECKSUM, INIT, FORMAT;
+
+
+-- Now let's restore
+RESTORE FILELISTONLY
+FROM DISK = 'C:\Demos\AdvRestore\DemoData_PITFull.bak';
+
+DROP DATABASE DemoData_Restored;
+
+RESTORE DATABASE DemoData_Restored
+FROM DISK = 'C:\Demos\AdvRestore\DemoData_PITFull.bak'
+WITH NORECOVERY,
+MOVE 'DemoData' TO 'C:\Demos\AdvRestore\DemoData-Restored.mdf',
+MOVE 'DemoData_log' TO 'C:\Demos\AdvRestore\DemoData-Restored.ldf',
+MOVE 'ZipCodeData' TO 'C:\Demos\AdvRestore\Demo_ZipCodeData-Restored.ndf',
+MOVE 'TowData' TO 'C:\Demos\AdvRestore\Demo_TowData-Restored.ndf';
+
+RESTORE LOG DemoData_Restored
+FROM DISK = 'C:\Demos\AdvRestore\DemoData_PIT1.trn'
+WITH NORECOVERY;
+
+-- RESTORE LOG with STOPAT
+RESTORE LOG DemoData_Restored
+FROM DISK = 'C:\Demos\AdvRestore\DemoData_PIT2.trn'
+WITH NORECOVERY,
+STOPAT = '2020-10-26 15:54:57.650';
+
+
+-- Recover
+RESTORE DATABASE DemoData_Restored WITH RECOVERY;
+
+--Backup Database
+BACKUP DATABASE DemoData_Restored
+TO DISK = 'C:\Demos\AdvRestore\DemoDataRestored_PITFull.bak'
+WITH COMPRESSION, CHECKSUM, INIT, FORMAT;
+
+-- Backup Log
+BACKUP LOG DemoData_Restored
+TO DISK = 'C:\Demos\AdvRestore\DemoDataRestored_PIT.trn'
+WITH COMPRESSION, CHECKSUM, INIT, FORMAT;
+
+-- now let's query
+USE [DemoData_Restored]
+GO
+
+SELECT *
+FROM dbo.ZipCodes;
+
+SELECT *
+FROM dbo.ZipCodes
+WHERE ZipCode = '60515'
+
+
+
+
+-- But what if you don't know the exact time the disaster occurred?
+USE [DemoData]
+GO
+
+SELECT [Current LSN], Operation, Context, AllocUnitName, [Page ID], [Begin Time], [Transaction Name]
+FROM fn_dblog(NULL, NULL);
+
+
+
+
+-- == MARKED TRANSACTIONS ==
+
+USE [DemoData_Restored]
+GO
+
+BEGIN TRAN UpdateState -- transaction name
+WITH MARK 'description_can_go_here' -- description is optional, "WITH MARK" is not
+
+UPDATE dbo.ZipCodes
+SET StateName = 'IL', ZipCode = '60513'
+WHERE ZipCode = '00000';
+
+COMMIT TRANSACTION UpdateState -- restate transaction name
+
+-- let's do another unmarked transaction
+UPDATE dbo.ZipCodes
+SET StateName = 'OR'
+WHERE StateName = 'WA';
+
+-- Backup Log
+BACKUP LOG DemoData_Restored
+TO DISK = 'C:\Demos\AdvRestore\DemoDataRestored_PIT2.trn'
+WITH COMPRESSION, CHECKSUM, INIT, FORMAT;
+
+
+-- Restore
+RESTORE DATABASE DemoData_Restored2
+FROM DISK = 'C:\Demos\AdvRestore\DemoDataRestored_PITFull.bak'
+WITH NORECOVERY,
+MOVE 'DemoData' TO 'C:\Demos\AdvRestore\DemoData-Restored2.mdf',
+MOVE 'DemoData_log' TO 'C:\Demos\AdvRestore\DemoData-Restored2.ldf',
+MOVE 'ZipCodeData' TO 'C:\Demos\AdvRestore\Demo_ZipCodeData-Restored2.ndf',
+MOVE 'TowData' TO 'C:\Demos\AdvRestore\Demo_TowData-Restored2.ndf';
+
+RESTORE LOG DemoData_Restored2
+FROM DISK = 'C:\Demos\AdvRestore\DemoDataRestored_PIT.trn'
+WITH NORECOVERY;
+
+RESTORE LOG DemoData_Restored2
+FROM DISK = 'C:\Demos\AdvRestore\DemoDataRestored_PIT2.trn'
+WITH NORECOVERY,
+STOPATMARK = 'UpdateState';-- apply all transactions UP TO AND INCLUDING this named transaction
+
+
+RESTORE DATABASE DemoData_Restored2
+WITH RECOVERY;
+
+USE [DemoData_Restored2]
+GO
+
+SELECT *
+FROM dbo.ZipCodes;
+
+SELECT *
+FROM dbo.ZipCodes
+WHERE ZipCode = '60513'
+
+-- See a list of all marked transactions
+SELECT * FROM msdb.dbo.logmarkhistory;
+
+
+
+-- Cleanup, Aisle 2
+USE [master]
+GO
+
+DROP DATABASE DemoData;
+DROP DATABASE DemoData_Restored;
+DROP DATABASE DemoData_Restored2;
+EXEC msdb.dbo.sp_delete_database_backuphistory 'DemoData'
+
---
+++ RecoveringWithMinimalImpact/README.md
@@ -0,0 +1,9 @@
+# Recovering With Minimal Impact: Advanced Database Restore Methods
+
+* [Demo Database](https://bobpusateri.blob.core.windows.net/shared/DemoData/DemoData.bak)
+
+
+### Abstract
+For mission-critical databases, backups are only half the battle – restores are what really matter when disaster strikes. Standard restores, while effective, may require additional downtime and thus further impact your business. This session will explain and demonstrate three advanced restore methods you should know: point-in-time restores, piecemeal restores, and page restores. You will walk away with a solid understanding of these methods, when they are appropriate, and what can be done to ensure they are always possible. Attend this session and gain senior-level skills via extra knowledge of how to minimize downtime and user impact after disaster has struck!
+
+_These materials are licensed under the [Creative Commons Attribution Non-Commercial ShareAlike license](https://creativecommons.org/licenses/by-nc-sa/4.0/), a localized version of which is available [inside this repository](https://github.com/BobPusateri/PresentationDemos/blob/master/License.md)._
